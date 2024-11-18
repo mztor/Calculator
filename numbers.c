ee2ae43ea4ea2a4455086615d1223d1d7b9da44f
[Sarah Tor] <stor@macarthur.nsw.edu.au>
2024-11-18 23:09:08 +1100

    Updated times table and added myEnglishName stub. Added comments for all the functions too.

--- numbers.c
+++ numbers.c
@@ -4,6 +4,7 @@ void oddOrEven(int n);
 void timesTable(int n);
 void square(int n);
 void prime(int n);
+void myEnglishName(int n);
 
 int main(void)
 {
@@ -32,15 +33,32 @@ void oddOrEven(int n)
 
 void timesTable(int n)
 {
+    //print the times table for n from 1 x n to 12 x n.
+    //first line has been started for you. Try the inefficient
+    //method first, then try using a loop
+
+    printf("%i x %i = %i\n", 1,n, 1*n);
     return;
 }
 
 void square(int n)
 {
+    //print out whether the number is a square number or not.
+    //if its square root is a floating point number, it is not square
+
     return;
 }
 
 void prime(int n)
 {
+    //print out whether the number is prime or not
+    return;
+}
+
+void myEnglishName(int n)
+{
+    //can you print out what n is in English? E.g.
+    //if the user typed 5, can you print Your name in English is five.
+    //you may assume that we won't exceed a hundred.
     return;
 }
